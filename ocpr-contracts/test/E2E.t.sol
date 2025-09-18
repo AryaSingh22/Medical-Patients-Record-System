66e253b071b3373ace283083798500335f57ae1e
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-09-18 22:56:57 +0530

    Update E2E.t.sol

--- ocpr-contracts/test/E2E.t.sol
+++ ocpr-contracts/test/E2E.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {RecordManager} from "../src/RecordManager.sol";    
 import {AccessManager} from "../src/AccessManager.sol";
-import {KeyManager} from "../src/KeyManager.sol";
+import {KeyManager} from "../src/KeyManager.sol";     
 import {PatientRegistry} from "../src/PatientRegistry.sol";
 import {AuditLog} from "../src/AuditLog.sol";
 
