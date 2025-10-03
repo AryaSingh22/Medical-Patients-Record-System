447272213f2d83af91f313d63347be4cefcda174
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-10-03 23:53:38 +0530

    Update KeyManager.sol

--- ocpr-contracts/src/KeyManager.sol
+++ ocpr-contracts/src/KeyManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.19; 
 
 import {RecordManager} from "./RecordManager.sol"; 
 import {PatientRegistry} from "./PatientRegistry.sol";
