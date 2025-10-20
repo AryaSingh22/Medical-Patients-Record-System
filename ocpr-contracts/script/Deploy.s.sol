62e5d1b3002355876d1537a105f5dce5acf69c15
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-10-20 22:34:46 +0530

    Update Deploy.s.sol

--- ocpr-contracts/script/Deploy.s.sol
+++ ocpr-contracts/script/Deploy.s.sol
@@ -12,7 +12,7 @@ import {AuditLog} from "../src/AuditLog.sol";
 contract Deploy is Script {
     function run() external { 
         // Load deployment key 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); 
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");  
         address deployer = vm.addr(deployerPrivateKey);  
         
         vm.startBroadcast(deployerPrivateKey);
