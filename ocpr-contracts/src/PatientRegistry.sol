db236a1e57ed9f39b1f183510ded1a77d0fb28ea
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-11-02 22:46:21 +0530

    Update PatientRegistry.sol

--- ocpr-contracts/src/PatientRegistry.sol
+++ ocpr-contracts/src/PatientRegistry.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
    
 /**
- * @title PatientRegistry
+ * @title PatientRegistry 
  * @notice Registers patients and links wallet ownership to patient IDs. 
  * @dev
  * - All personally identifiable information (PII) must be salted-hashed before being stored on-chain.
