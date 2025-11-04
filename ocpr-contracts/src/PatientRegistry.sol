945f64af3555aa72ca628095d6f069b43c6fa0cf
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-11-04 23:56:14 +0530

    Update PatientRegistry.sol

--- ocpr-contracts/src/PatientRegistry.sol
+++ ocpr-contracts/src/PatientRegistry.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol"; 
-     
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /** 
  * @title PatientRegistry 
  * @notice Registers patients and links wallet ownership to patient IDs.  
