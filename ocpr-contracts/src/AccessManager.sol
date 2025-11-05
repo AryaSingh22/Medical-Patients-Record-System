d12e7e929c80ef5aaac80f29b8fd89e257eea108
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-11-05 23:40:20 +0530

    Update AccessManager.sol

--- ocpr-contracts/src/AccessManager.sol
+++ ocpr-contracts/src/AccessManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";  
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";   
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";  
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {IAccessManager} from "../access/IAccessManager.sol"; 
