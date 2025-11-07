5b475d8c6c0fd9ff004bfedc904eb10e25e9b165
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-11-07 23:41:03 +0530

    Update AuditLog.sol

--- ocpr-contracts/src/AuditLog.sol
+++ ocpr-contracts/src/AuditLog.sol
@@ -6,6 +6,6 @@ contract AuditLog {
     event Audit(bytes32 indexed actionHash, address indexed actor, uint64 ts);
  
     function emitAudit(bytes32 actionHash) external {
-        emit Audit(actionHash, msg.sender, uint64(block.timestamp));       
+        emit Audit(actionHash, msg.sender, uint64(block.timestamp));
     } 
 }   
